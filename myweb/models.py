a4c7978f0592ee026fb842b1d52ff580d27cb39d
somya1150 <somya.ran2106@gmail.com>
2026-04-17 15:54:28 +0530

    Add Task model with title and created_at fields

--- myweb/models.py
+++ myweb/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  created_at = models.DateTimeField(auto_now_add=True)
+  def__str__(self):
+        return self.title
