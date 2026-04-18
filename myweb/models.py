409230c553838e0318c03921ef02ab38d0424234
somya1150 <somya.ran2106@gmail.com>
2026-04-18 20:43:03 +0530

    Update models.py

--- myweb/models.py
+++ myweb/models.py
@@ -13,10 +13,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    # This links the profile to a specific user
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
-    # Your campus-specific fields
+
     college_id = models.CharField(max_length=20)
     semester = models.CharField(max_length=10)
     phone_no = models.CharField(max_length=15)
