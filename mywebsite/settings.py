e3dfa61116a5e195f2cda3a4261a99a39eb50eeb
somya1150 <somya.ran2106@gmail.com>
2026-04-19 01:12:57 +0530

    Update Django settings for static files management
    Add static files settings for deployment

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -116,9 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
