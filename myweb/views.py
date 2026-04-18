0332b92eacfc0e3f0b4ff682b10517f193a71bb9
somya1150 <somya.ran2106@gmail.com>
2026-04-18 22:20:12 +0530

    Fix formatting and remove duplicate login_page function

--- myweb/views.py
+++ myweb/views.py
@@ -7,13 +7,11 @@ from django.contrib import messages
 
 def register_user(request):
     if request.method == 'POST':
-        # Grabbing data using the 'name' attributes you just added to HTML
         u_name = request.POST.get('username')
         u_email = request.POST.get('email')
         u_pass = request.POST.get('password')
         college_id = request.POST.get('college_id') # New field!
         
-        # 1. Create the basic User
         if User.objects.filter(username=u_name).exists():
             messages.error(request, "Username already taken")
             return render(request, 'todo/login.html')
@@ -21,7 +19,6 @@ def register_user(request):
         user = User.objects.create_user(username=u_name, email=u_email, password=u_pass)
         user.save()
         
-        # 2. Redirect to login after successful registration
         messages.success(request, "Account created! Please login.")
         return redirect('myweb:login')
 
