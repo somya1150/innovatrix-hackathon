c4114faa3442f003a96cee24aaeda817fedc2935
somya1150 <somya.ran2106@gmail.com>
2026-04-18 19:31:27 +0530

    Add files via upload

--- myweb/views.py
+++ myweb/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def register_user(request):
+    if request.method == 'POST':
+        # Grabbing data using the 'name' attributes you just added to HTML
+        u_name = request.POST.get('username')
+        u_email = request.POST.get('email')
+        u_pass = request.POST.get('password')
+        college_id = request.POST.get('college_id') # New field!
+        
+        # 1. Create the basic User
+        if User.objects.filter(username=u_name).exists():
+            messages.error(request, "Username already taken")
+            return render(request, 'todo/login.html')
+            
+        user = User.objects.create_user(username=u_name, email=u_email, password=u_pass)
+        user.save()
+        
+        # 2. Redirect to login after successful registration
+        messages.success(request, "Account created! Please login.")
+        return redirect('myweb:login')
+
+    return render(request, 'todo/login.html')
+
+def index(request):
+    return render(request,'todo/index.html')
+def login_page(request):
+    return render(request, 'todo/login.html')
+
+def login_page(request):
+    return render(request,'todo/login.html')
