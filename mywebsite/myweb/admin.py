c180a0ac5400252098862c558fcc4704e7b17ff5
somya1150 <somya.ran2106@gmail.com>
2026-08-11 15:11:47 +0530

    Remove duplicate nested files and folders

--- mywebsite/myweb/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from myweb.models import login
-
-
-#this is the admin panel for the login model created in models.py file. It will show the username, email, college_id and phone number of the user in the admin panel.
-class LoginAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'college_id', 'phone')
-
-admin.site.register(login, LoginAdmin)
