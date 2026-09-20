b863af7a4f837f962bbd28a331be5528e8077625
somya1150 <somya.ran2106@gmail.com>
2026-09-21 00:36:16 +0530

    FIXED IMAGE LOADING INSIDE ADMIN

--- myweb/urls.py
+++ myweb/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'myweb'
 
@@ -15,3 +17,6 @@ urlpatterns = [
     path('logout/',views.logoutuser,name='logout'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
